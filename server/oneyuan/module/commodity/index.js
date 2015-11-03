522b50fc008503005fdb1e8268f1e324b8050620
Jiang Lingfeng <jianglingfeng@iyunxiao.com>
2015-11-04 00:36:38 +0800

    server: add listType and detailCommodity

--- server/oneyuan/module/commodity/index.js
+++ server/oneyuan/module/commodity/index.js
@@ -8,19 +8,40 @@ var mysqlConnection = require('mysql').createConnection(config.get('mysql'));
 
 /**
  * 添加t类型*/
-function addType(){
+/**
+ * 手机平板 10
+ * 电脑办公 11
+ * 数码影音 12
+ * 女性时尚 20
+ * 美食天地 30
+ * 潮流新品 40
+ * 其他商品 90
+ */
+function addType(data, callback) {
   var data = {
-    id:new ObjectID().toString(),
-    code:'10',
-    typeName:'手机平板',
+    id: new ObjectID().toString(),
+    code: '90',
+    typeName: '其他商品',
   };
+  mysqlConnection.query('insert into type(`id`,`code`,`typeName`) values(?,?,?)', [data.id, data.code, data.typeName], function (err, rows) {
+    if (err) {
+      callback(err, null);
+    }
+    console.log(rows);
+    (null, rows);
+  });
 }
 
-function listType(data, callback){
-
+function listType(data, callback) {
+  mysqlConnection.query('select code, typeName as Name from type', function (err, rows) {
+    if (err) {
+      callback(err, null);
+    }
+    callback(null, rows);
+  });
 }
 
- /**
+/**
  * 增加商品*/
 function addCommodity(data, callback) {
   var now = new Date().getTime();
@@ -28,31 +49,45 @@ function addCommodity(data, callback) {
     id: new ObjectID().toString(),
     name: 'Apple iPhone6s Plus 64G 颜色随机 唯一的不同，就是处处不同',
     price: 8080,
-    type: '10',
+    typeCode: '10',
     label: 'iphone6s 苹果',
     imageList: 'http://res.126.net/p/dbqb/one/148/898/2e9c1febb2ae050e1729ec54ebcb0075.jpg',
     createTime: now,
     updateTime: now,
   };
-   mysqlConnection.query('select 1+1 as ss',function(err, rows){
-     console.log(rows);
-   });
+  mysqlConnection.query('insert into commodity(`id`,`name`,`price`,`typeCode`,`label`,`imageList`,`createTime`,`updateTime`) ' +
+  'values(?,?,?,?,?,?,?,?)', [data.id, data.name, data.price, data.typeCode, data.label, data.imageList, data.createTime, data.updateTime], function (err, rows) {
+    console.log(err);
+    callback(null, rows);
+  });
+}
 
-//   query.on('error', function(err){
-//     console.log("dafads"+err);
-//   });
+function listCommodity(data, callback){
+  console.log(data);
+  mysqlConnection.query('select * from commodity where `typeCode` = ? limit ?,?', [data.typeCode, data.start, data.end], function (err, rows) {
+    console.log(err);
+    callback(null, rows);
+  });
 }
 
 function detail(data, callback) {
-
   if (_.isEmpty(data.id)) {
     console.log('id is Empty!');
     return res.json("error");
   }
+  mysqlConnection.query('select * from commodity where `id`=?', [data.id], function (err, rows) {
+    if (err) {
+      callback(err, null);
+    }
+    callback(null, rows[0]);
+  });
+
 }
 
 module.exports = {
-  addCommodity: addCommodity,
-  detail: detail,
+  addType: addType,
   listType: listType,
+  addCommodity: addCommodity,
+  listCommodity:listCommodity,
+  detail: detail,
 }
--- server/oneyuan/routes/commodity.js
+++ server/oneyuan/routes/commodity.js
@@ -1,6 +1,7 @@
 /**
  * Created by jlf on 15/10/30.
  */
+var _ = require('underscore');
 var router = require('express').Router();
 var commodityService = require('../module/commodity');
 var errorHandler = require('../common/error_handler');
@@ -11,12 +12,17 @@ router.get('/types', function(req, res, next) {
     SSID: req.cookies.SSID || '',
   };
 
-  commodityService.listType(data, function (err, result) {
+  commodityService.listType(data, function (err, typeList) {
     if (err) {
       var error = new Error();
       error.message = err;
       errorHandler(res, error, error_code.DatabaseQueryError);
     }
+    var result = {
+      code:error_code.Success,
+      msg:err,
+      data:typeList,
+    };
     return res.json(result);
   });
 });
@@ -27,24 +33,59 @@ router.get('/:id', function(req, res, next) {
     SSID: req.cookies.SSID || '',
   };
 
-  commodityService.addCommodity(data, function(err, result) {
+  commodityService.detail(data, function(err, commodity) {
     if (err) {
       var error = new Error();
       error.message = err;
       errorHandler(res, error, error_code.DatabaseQueryError);
     }
+    var result = {
+      code:error_code.Success,
+      msg:err,
+      data:commodity,
+    };
+    return res.json(result);
   });
 });
+
 router.get('/:id/records', function(req, res, next) {
 
 });
-router.post('/buy');
-router.get('/', function(req, res, next){
+
+router.get('/*', function(req, res, next){
   var data = {
     typeCode: req.query.typeCode || '',
     pageNo: req.query.pageNo || '',
     pageSize: req.query.pageSize || '',
     SSID: req.cookies.SSID || '',
   }
+
+  if(_.isEmpty(data.pageSize)){
+    var error = new Error();
+    error.message = "Params pageSize is Empty!";
+    errorHandler(res, error, error_code.ParamsError);
+  }
+
+  if(_.isEmpty(data.pageNo)){
+    data.pageNo = 1;
+  }
+
+  data.start = (parseInt(data.pageNo) - 1) * data.pageSize;
+  data.end = parseInt(data.pageNo) * data.pageSize;
+
+  commodityService.listCommodity(data, function(err, commodities){
+    if(err){
+      var error = new Error();
+      error.message = err;
+      errorHandler(res, error, error_code.DatabaseQueryError);
+    }
+    var result = {
+      code:error_code.Success,
+      msg:err,
+      data:commodities,
+    };
+    return res.json(result);
+  });
 });
+
 module.exports = router;
